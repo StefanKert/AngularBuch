d057e523fd083ee3924d6c7ed8b35923648136ef
Stefan Kert <stefan.kert@sbg.at>
2017-05-28 16:31:22 +0200

    finished 6.2

---
+++ BookMonkey/src/app/book-list-item/book-list-item.component.ts
@@ -0,0 +1,16 @@
+import { Book } from './../shared/book';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'a.bm-book-list-item',
+  templateUrl: './book-list-item.component.html',
+  styles: []
+})
+export class BookListItemComponent implements OnInit {
+
+  @Input() book: Book;
+
+  constructor() { }
+
+  ngOnInit() { }
+}
